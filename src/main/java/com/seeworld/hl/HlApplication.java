d481c77b23c18534fb33b25a2bc852125b8c502d
铿然 <thd0106@163.com>
2019-05-05 16:09:12 +0800

    Initial commit

---
+++ src/main/java/com/seeworld/hl/HlApplication.java
@@ -0,0 +1,17 @@
+package com.seeworld.hl;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@MapperScan(value = "com.seeworld.hl.dao")
+@ComponentScan(basePackages = {"com.seeworld.hl.*"})
+public class HlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HlApplication.class, args);
+    }
+
+}
