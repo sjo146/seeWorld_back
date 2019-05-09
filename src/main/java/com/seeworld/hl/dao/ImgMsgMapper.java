da322105392fbd9be3ce81e30864d61bcd957a9c
铿然 <thd0106@163.com>
2019-05-09 08:59:18 +0800

    修改了配置文件

--- src/main/java/com/seeworld/hl/dao/ImgMsgMapper.java
+++ src/main/java/com/seeworld/hl/dao/ImgMsgMapper.java
@@ -13,9 +13,10 @@ import java.util.ArrayList;
 @Component
 @Mapper
 public interface ImgMsgMapper {
-@Select("select* from ImgMsg")
+    @Select("select* from ImgMsg")
     ArrayList<ImgMsg> findAllImgMsg();
+
     @Insert("insert into ImgMsg(img_type,img_title,img_desc,img_asset_name,img_resource_name) values (#{ImgType},#{ImgTitle},#{ImgDesc},#{ImgAssetName},#{ImgResourceName})")
-    @Options(useGeneratedKeys = true,keyProperty = "ImgId",keyColumn = "img_id")
+    @Options(useGeneratedKeys = true, keyProperty = "ImgId", keyColumn = "img_id")
     int addImg(ImgMsg img);
 }
